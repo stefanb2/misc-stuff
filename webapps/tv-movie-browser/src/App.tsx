f0c15f18fb21e0505846835c44c826d38d726e7a
Stefan Becker <chemobejk@gmail.com>
2021-08-15 19:37:00 +0300

    tv-movie-browser: add router to application

--- webapps/tv-movie-browser/src/App.tsx
+++ webapps/tv-movie-browser/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import {Main} from './components/Main';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        TBD...
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:date/:providerId/:selectorId" component={Main} />
+          <Route path="/:date/:providerId" component={Main} />
+          <Route path="/:date" component={Main} />
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
