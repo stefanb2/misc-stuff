3409796e3ee912174b64977fcfb0cbbe53a1beba
Stefan Becker <chemobejk@gmail.com>
2021-09-05 17:54:30 +0300

    tv-movie-browser: implement selectors() for telkku
    Fetch channel groups from API endpoint and map it to selectors map.

--- webapps/tv-movie-browser/src/slices/provider/telkku.ts
+++ webapps/tv-movie-browser/src/slices/provider/telkku.ts
@@ -1,12 +1,33 @@
+import {get} from '../../utils/apiRequest'
+
+const apiPath = 'https://www.telkku.com/api'
+
 const program = async (day: Day, selectorId: string) => {
   return []
 }
 
-const selectors = async () => {
-  return {}
+type ChannelGroup = {
+  name: string
+  slug: string
 }
 
-const provider = {
+type ChannelGroupsAPIResponse = {
+  response: ChannelGroup[]
+}
+
+const selectors = async () => {
+  const {response: groups} = await get(`${apiPath}/channel-groups`) as ChannelGroupsAPIResponse
+
+  return groups.reduce<SelectorMap>(
+    (a, {name: id, slug}) => {
+      a[slug] = {id, slug}
+      return a
+    },
+    {}
+  )
+}
+
+const provider: Provider = {
   name: 'telkku',
   program,
   selectors,
