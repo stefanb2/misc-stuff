a8755d094911bbdc067bf96c9cab18c17ef92883
Stefan Becker <chemobejk@gmail.com>
2025-02-24 12:00:39 +0200

    comics: switch Viivi ja Wagner to new HS fetcher
    - determined API URL from web page using browser developer tools

--- comics/comics-fetch.d/45_viiviwagner.pm
+++ comics/comics-fetch.d/45_viiviwagner.pm
@@ -4,29 +4,9 @@ use strict;
 use warnings;
 
 sub get() {
-    ::get_comic(
-        'https://www.hs.fi/viivijawagner/',
- 	    'viiviwagner',
-        30,
-        sub {
-            my($tree) = @_;
-            my $url;
-
-            my $node = $tree->look_down(class => 'cartoon-content');
-            if ($node) {
-                my $figure = $node->find('figure');
-                if ($figure) {
-                    my $img = $figure->find('img');
-                    if ($img) {
-                        my $src = $img->attr('data-srcset');
-                        ($url = (split(' ', $src))[0]) =~ s,^//,https://,
-                            if $src;
-                    }
-                }
-            }
-
-            return($url ? $url : ());
-        }
+    ::get_comic_hs(
+        'https://www.hs.fi/api/laneitems/39221/list/normal/291',
+        'viiviwagner'
     );
 }
 
